e513a75fee926de3754f7ca03dd4a90ae4b5ea99
Taha <115118538+Taha5885@users.noreply.github.com>
2023-06-11 15:43:43 +0300

    Update Project_1.sql

--- SQL_PROJELERİ/Project_1.sql
+++ SQL_PROJELERİ/Project_1.sql
@@ -9,23 +9,23 @@
 
 
 --1.soru
---SELECT title,description FROM film;
+SELECT title,description FROM film;
 
 --2.soru
---SELECT * FROM film 
---WHERE length > 60 AND length < 75; 
+SELECT * FROM film 
+WHERE length > 60 AND length < 75; 
 
 --3.soru	
---SELECT * FROM film
---WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99
+SELECT * FROM film
+WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99
 
 --4.soru
---SELECT * FROM customer
---WHERE first_name = 'Mary' 
+SELECT * FROM customer
+WHERE first_name = 'Mary' 
 
 --5.soru
---SELECT * FROM film 
---where not length > 50 AND rental_rate != 2.99 AND rental_rate != 4.99;
+SELECT * FROM film 
+where not length > 50 AND rental_rate != 2.99 AND rental_rate != 4.99;
 
 
 
